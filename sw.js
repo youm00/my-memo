b49f7c68017864aa497753b59d91dba8ed0acfae
youm00 <44356792+youm00@users.noreply.github.com>
2026-03-02 18:39:50 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -5,31 +5,25 @@ const APP_SHELL = [
   './manifest.json'
 ];
 
-// install
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(APP_SHELL);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL))
   );
   self.skipWaiting();
 });
 
-// activate
 self.addEventListener('activate', event => {
   event.waitUntil(self.clients.claim());
 });
 
-// fetch
 self.addEventListener('fetch', event => {
   const url = new URL(event.request.url);
 
-  // GitHub APIはキャッシュしない
   if (url.hostname.includes('api.github.com')) return;
 
   event.respondWith(
-    caches.match(event.request).then(cached => {
-      return cached || fetch(event.request);
+    caches.match(event.request).then(res => {
+      return res || fetch(event.request);
     })
   );
 });
