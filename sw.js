f47425fef24c884ee5168812137039484172309b
youm00 <44356792+youm00@users.noreply.github.com>
2026-03-04 13:48:47 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,35 @@
-// disabled
+const CACHE_NAME = 'gist-memo-v6';
+const APP_SHELL = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// install
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(APP_SHELL);
+    })
+  );
+  self.skipWaiting();
+});
+
+// activate
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+// fetch
+self.addEventListener('fetch', event => {
+  const url = new URL(event.request.url);
+
+  // GitHub APIはキャッシュしない
+  if (url.hostname.includes('api.github.com')) return;
+
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request);
+    })
+  );
+});
