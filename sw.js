d18fc3c3b7de0a691003efd24493b503b1209674
youm00 <44356792+youm00@users.noreply.github.com>
2026-03-04 12:16:06 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,35 +1,13 @@
-const CACHE_NAME = 'gist-memo-v6';
-const APP_SHELL = [
-  './',
-  './index.html',
-  './manifest.json'
-];
-
-// install
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(APP_SHELL);
-    })
-  );
+self.addEventListener('install', function() {
   self.skipWaiting();
 });
 
-// activate
-self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
-});
-
-// fetch
-self.addEventListener('fetch', event => {
-  const url = new URL(event.request.url);
-
-  // GitHub APIはキャッシュしない
-  if (url.hostname.includes('api.github.com')) return;
-
-  event.respondWith(
-    caches.match(event.request).then(cached => {
-      return cached || fetch(event.request);
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    self.registration.unregister().then(function() {
+      return self.clients.matchAll();
+    }).then(function(clients) {
+      clients.forEach(client => client.navigate(client.url));
     })
   );
 });
